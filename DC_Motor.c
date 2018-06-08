99268aff8bb6394ec4f6393e8b336d8a90c41b48
Yash Kelkar <ykelk001@ucr.edu>
2018-06-08 04:43:39 -0700

    added comments

--- DC_Motor.c
+++ DC_Motor.c
@@ -63,39 +63,11 @@ void TimerSet(unsigned long M)
 
 void InitPWM()
 {
-   /*
-   TCCR0 - Timer Counter Control Register (TIMER0)
-   -----------------------------------------------
-   BITS DESCRIPTION
-   
-
-   NO:   NAME   DESCRIPTION
-   --------------------------
-   BIT 7 : FOC0   Force Output Compare [Not used in this example]
-   BIT 6 : WGM00  Wave form generation mode [SET to 1]
-   BIT 5 : COM01  Compare Output Mode        [SET to 1]
-   BIT 4 : COM00  Compare Output Mode        [SET to 0]
-
-   BIT 3 : WGM01  Wave form generation mode [SET to 1]
-   BIT 2 : CS02   Clock Select               [SET to 0]
-   BIT 1 : CS01   Clock Select               [SET to 0]
-   BIT 0 : CS00   Clock Select               [SET to 1]
-
-   The above settings are for
-   --------------------------
-
-   Timer Clock = CPU Clock (No Prescaling)
-   Mode        = Fast PWM
-   PWM Output  = Non Inverted
-
-   */
-
-
-	TCCR0A = (1 << COM0A1 | 1 << COM0A0 | 1 << WGM00 | 1 << WGM01); // Toggle OC1A on Compare Match Fast PWM
-	TCCR0B = (1 << CS00);	// No prescalar
+ 	TCCR0A = (1 << COM0A1 | 1 << COM0A0 | 1 << WGM00 | 1 << WGM01); // Toggle OC1A on Compare Match Fast PWM
+	TCCR0B = (1 << CS00);	
 
 	TCCR2A = (1 << COM2A1 | 1 << COM2A0 | 1 << WGM20 | 1 << WGM21);
-	TCCR2B = (1 << CS20); //OC2A
+	TCCR2B = (1 << CS20); //Toggle OC2A on Compare Match Fast PWM
 	
 }
 unsigned char PD5_high = 255;
